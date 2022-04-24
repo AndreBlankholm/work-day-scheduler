63c8f8754d3eeeb14c5c98891c2a0c5d1a8fb155
AndreBlankholm <ablankholm@gmail.com>
2022-04-24 11:27:10 -0500

    getting help

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,49 @@
-//make var for button
-var saveBtn = document.querySelector(".saveBtn");
+// Assign saveBtn to button variable
+var saveButton = $(".saveBtn");
 
+// .ready() Runs everything before user sees it
+// Displays current date
 $(document).ready(function() {
-  var date = moment().format("MMMM Do YYYY, h:mm:ss a");
-  $("#currentDay").text(date);
+    var currentDate = moment().format("dddd MMMM Do");
+    $("#currentDay").text(currentDate);
 
-  function currentTime() {
-    var current = moment().hours();
+    // Time Block Colors: Past = Grey || Present = Red || Future = Green 
+    function currentTime() {
+        var current = moment().hours()
+        var time = $(".time-block");
+        time.each(function() {
+            var hour = parseInt($(this).attr("id"))
+            if (hour === current) {
+                $(this).children(".col-10").attr("class", "present col-10 description")
+            } else if (current > hour) {
+                $(this).children(".col-10").attr("class", "past col-10 description")
+            } else {
+                $(this).children(".col-10").attr("class", "future col-10 description")
+            }
+        })
+    };
+    currentTime()
 
-    var time = $(".time-block");
-    
-    time.each(function() {
-      var hour = parseInt($(this).attr("id"));
-      if (hour === current) {
-        $(this).children(".col-10").attr("class", "present col-10 description");
-      } else if (current > hour) {
-        $(this).children(".col-10").attr("class", "past col-10 description");
-      } else {
-        $(this).children(".col-10").attr("class", "future col-10 description");
-      }
+    //Local Storage for saving scheduled events
+    saveButton.on("click", function(event) {
+        event.preventDefault();
+        var description = $(this).siblings(".col-10").val().replace(/[""]+/g, "");
+        var parentEl = $(this).parent().attr("id");
+
+        localStorage.setItem(parentEl, JSON.stringify(description));
     });
-  };
 
-
-  button.on("click", function(event) {
-    event.preventDefault();
-    var text = $(this).siblings(".col-10").val().replace(/[""]+/g, "");
-    var parent = $(this).parent().attr("id");
-
-    localStorage.setItem(parent, JSON.stringify(text));
-  });
-
-  $("#9").val(localStorage.getItem("9").replace(/[""]+/g, ""));
-  $("#10").val(localStorage.getItem("10").replace(/[""]+/g, ""));
-  $("#11").val(localStorage.getItem("11").replace(/[""]+/g, ""));
-  $("#12").val(localStorage.getItem("12").replace(/[""]+/g, ""));
-  $("#13").val(localStorage.getItem("13").replace(/[""]+/g, ""));
-  $("#14").val(localStorage.getItem("14").replace(/[""]+/g, ""));
-  $("#15").val(localStorage.getItem("15").replace(/[""]+/g, ""));
-  $("#16").val(localStorage.getItem("16").replace(/[""]+/g, ""));
-  $("#17").val(localStorage.getItem("17").replace(/[""]+/g, ""));
-
-  currentTime();
-
+    // Replacing textareas with 'global' saved for that row
+    // (/[""]+/g, "") uses Global Modifier to display everything that was previously saved
+    $("#9 textarea").val(localStorage.getItem("9").replace(/[""]+/g, ""));
+    $("#10 textarea").val(localStorage.getItem("10").replace(/[""]+/g, ""));
+    $("#11 textarea").val(localStorage.getItem("11").replace(/[""]+/g, ""));
+    $("#12 textarea").val(localStorage.getItem("12").replace(/[""]+/g, ""));
+    $("#13 textarea").val(localStorage.getItem("13").replace(/[""]+/g, ""));
+    $("#14 textarea").val(localStorage.getItem("14").replace(/[""]+/g, ""));
+    $("#15 textarea").val(localStorage.getItem("15").replace(/[""]+/g, ""));
+    $("#16 textarea").val(localStorage.getItem("16").replace(/[""]+/g, ""));
+    $("#17 textarea").val(localStorage.getItem("17").replace(/[""]+/g, ""));
 });
+
+
