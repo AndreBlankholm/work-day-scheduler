e21441cc03b454fa8b224097ca695c60d81cda0a
AndreBlankholm <ablankholm@gmail.com>
2022-04-24 12:54:53 -0500

    still looking for solutions to my storage and retrive problems

--- assets/script.js
+++ assets/script.js
@@ -14,36 +14,37 @@ $(document).ready(function() {
         time.each(function() {
             var hour = parseInt($(this).attr("id"))
             if (hour === current) {
-                $(this).children(".col-10").attr("class", "present col-10 description")
+                $(this).children(".col-10").attr("class", "present col-10 description");
             } else if (current > hour) {
-                $(this).children(".col-10").attr("class", "past col-10 description")
+                $(this).children(".col-10").attr("class", "past col-10 description");
             } else {
-                $(this).children(".col-10").attr("class", "future col-10 description")
+                $(this).children(".col-10").attr("class", "future col-10 description");
             }
-        })
+        });
     };
+
     currentTime()
 
     //Local Storage for saving scheduled events
     saveButton.on("click", function(event) {
         event.preventDefault();
-        var description = $(this).siblings(".col-10").val().replace(/[""]+/g, "");
+        var description = $(this).siblings(".col-10").val().replace(parentEl);
         var parentEl = $(this).parent().attr("id");
 
         localStorage.setItem(parentEl, JSON.stringify(description));
     });
 
-    // Replacing textareas with 'global' saved for that row
-    // (/[""]+/g, "") uses Global Modifier to display everything that was previously saved
-    $("#9 textarea").val(localStorage.getItem("9").replace(/[""]+/g, ""));
-    $("#10 textarea").val(localStorage.getItem("10").replace(/[""]+/g, ""));
-    $("#11 textarea").val(localStorage.getItem("11").replace(/[""]+/g, ""));
-    $("#12 textarea").val(localStorage.getItem("12").replace(/[""]+/g, ""));
-    $("#13 textarea").val(localStorage.getItem("13").replace(/[""]+/g, ""));
-    $("#14 textarea").val(localStorage.getItem("14").replace(/[""]+/g, ""));
-    $("#15 textarea").val(localStorage.getItem("15").replace(/[""]+/g, ""));
-    $("#16 textarea").val(localStorage.getItem("16").replace(/[""]+/g, ""));
-    $("#17 textarea").val(localStorage.getItem("17").replace(/[""]+/g, ""));
+    
+    
+    $("#9 textarea").val(localStorage.getItem("9"));
+    $("#10 textarea").val(localStorage.getItem("10"));
+    $("#11 textarea").val(localStorage.getItem("11"));
+    $("#12 textarea").val(localStorage.getItem("12"));
+    $("#13 textarea").val(localStorage.getItem("13"));
+    $("#14 textarea").val(localStorage.getItem("14"));
+    $("#15 textarea").val(localStorage.getItem("15"));
+    $("#16 textarea").val(localStorage.getItem("16"));
+    $("#17 textarea").val(localStorage.getItem("17"));
 });
 
 
