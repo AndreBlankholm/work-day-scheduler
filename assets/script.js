d4b62006ea0198864f9c2304a1f16b0f7dd57d11
AndreBlankholm <ablankholm@gmail.com>
2022-04-24 14:50:22 -0500

    added json parse and some styling to the hours area

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
-// Assign saveBtn to button variable
+//saveBtn to button variable
 var saveButton = $(".saveBtn");
 
-// .ready() Runs everything before user sees it
-// Displays current date
+// 
+// displays current date
 $(document).ready(function() {
     var currentDate = moment().format("dddd MMMM Do");
     $("#currentDay").text(currentDate);
 
-    // Time Block Colors: Past = Grey || Present = Red || Future = Green 
+    
     function currentTime() {
         var current = moment().hours()
         var time = $(".time-block");
         time.each(function() {
+            console.log(this);
             var hour = parseInt($(this).attr("id"))
             if (hour === current) {
                 $(this).children(".col-10").attr("class", "present col-10 description");
@@ -35,16 +36,16 @@ $(document).ready(function() {
     });
 
     
-    
-    $("#9 textarea").val(localStorage.getItem("9"));
-    $("#10 textarea").val(localStorage.getItem("10"));
-    $("#11 textarea").val(localStorage.getItem("11"));
-    $("#12 textarea").val(localStorage.getItem("12"));
-    $("#13 textarea").val(localStorage.getItem("13"));
-    $("#14 textarea").val(localStorage.getItem("14"));
-    $("#15 textarea").val(localStorage.getItem("15"));
-    $("#16 textarea").val(localStorage.getItem("16"));
-    $("#17 textarea").val(localStorage.getItem("17"));
+    // added json parse
+    $("#9 textarea").val(JSON.parse(localStorage.getItem("9")));
+    $("#10 textarea").val(JSON.parse(localStorage.getItem("10")));
+    $("#11 textarea").val(JSON.parse(localStorage.getItem("11")));
+    $("#12 textarea").val(JSON.parse(localStorage.getItem("12")));
+    $("#13 textarea").val(JSON.parse(localStorage.getItem("13")));
+    $("#14 textarea").val(JSON.parse(localStorage.getItem("14")));
+    $("#15 textarea").val(JSON.parse(localStorage.getItem("15")));
+    $("#16 textarea").val(JSON.parse(localStorage.getItem("16")));
+    $("#17 textarea").val(JSON.parse(localStorage.getItem("17")));
 });
 
 
